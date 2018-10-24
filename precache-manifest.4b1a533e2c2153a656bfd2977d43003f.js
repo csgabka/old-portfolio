8f06bd1d3c9860627d1f4c4dd0c03d7407754ffd
Gabriella <csgabka@users.noreply.github.com>
2018-10-24 14:39:39 +0100

    Updates

---
+++ precache-manifest.4b1a533e2c2153a656bfd2977d43003f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a38a82f03c4d95c91e7253469c78de9",
+    "url": "/csgabka-portfolio/static/media/port1_scrs.0a38a82f.png"
+  },
+  {
+    "revision": "bbcfd54b905cc1f959fd949e851a1465",
+    "url": "/csgabka-portfolio/static/media/intro_bg2.bbcfd54b.jpg"
+  },
+  {
+    "revision": "820f61d6dd9fe1f9a59e7202ad49fc2f",
+    "url": "/csgabka-portfolio/static/media/17311080_10211117562268372_3587118879478260351_o.820f61d6.jpg"
+  },
+  {
+    "revision": "7c212377db82d07bef7e",
+    "url": "/csgabka-portfolio/static/js/runtime~main.7c212377.js"
+  },
+  {
+    "revision": "468ccd9830019a083e19",
+    "url": "/csgabka-portfolio/static/js/main.468ccd98.chunk.js"
+  },
+  {
+    "revision": "2eeade8d8b889d145b86",
+    "url": "/csgabka-portfolio/static/js/1.2eeade8d.chunk.js"
+  },
+  {
+    "revision": "468ccd9830019a083e19",
+    "url": "/csgabka-portfolio/static/css/main.080abe37.chunk.css"
+  },
+  {
+    "revision": "c5d96046221354eb0144346185031e20",
+    "url": "/csgabka-portfolio/index.html"
+  }
+];
