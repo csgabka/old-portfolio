832e7aa6663c6ae3269186502545bf48f6793b64
Gabriella <csgabka@users.noreply.github.com>
2018-10-24 19:36:10 +0100

    particles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import '../css/Contact.css';
-import axios from 'axios';
-import Popup from "reactjs-popup";
+import Particles from 'react-particles-js';
 
 class Contact extends Component {
 	
 	render() {
+		const particleOptions = {
+            		particles: {
+            			line_linked: {
+            				shadow: {
+            					enable: true,
+            					color: "#3CA9D1",
+            					blur: 5
+            				}
+            			}
+            		}
+            	}
 		return (
 		<div className="contact">
 		<h1>Connect me on social media</h1>
+		<div>
+			
+		<Particles 
+              params={particleOptions}
+            />
+
+
+
+		</div>
+		
 		<hr />
 		<button id="scroll-button" type="button" className="btn btn-outline-info" onClick={this.props.scrollToTop}>
 		Scroll me back to the top!</button>
