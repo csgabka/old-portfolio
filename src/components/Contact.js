9ae9382066f012836ef6c64c1833cbea1082286a
Gabriella <csgabka@users.noreply.github.com>
2018-10-25 09:05:06 +0100

    new contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import '../css/Contact.css';
-import Particles from 'react-particles-js';
+//import Particles from 'react-particles-js';
+import FrontSide from '../img/psx_20181024_223039.jpg';
+import BackSide from '../img/psx_20181024_223123.jpg';
+import LeftSide from '../img/psx_20181024_223143.jpg';
+import RightSide from '../img/psx_20181024_223422.jpg';
 
 class Contact extends Component {
 	
 	render() {
-		const particleOptions = {
+		{/*const particleOptions = {
             		particles: {
             			line_linked: {
             				shadow: {
@@ -16,14 +20,37 @@ class Contact extends Component {
             			}
             		}
             	}
+
+           <Particles params={particleOptions}  /> 	*/}
 		return (
 		<div className="contact">
 		<h1>Connect me on social media</h1>
 		<div>
 			
-		<Particles 
-              params={particleOptions}
-            />
+		
+		<div class="rotating_box">
+			<div class="single_rb">
+				<div class="front_side">
+					<img src={FrontSide} alt="" />
+				</div>
+				<div class="back_side">
+					<img src={BackSide} alt="" />
+				</div>
+				<div class="left_side">
+					<img src={LeftSide} alt="" />
+				</div>
+				<div class="right_side">
+					<img src={RightSide} alt="" />
+				</div>
+				<div class="top_side">
+					<img src={FrontSide} alt="" />
+				</div>
+				<div class="bottom_side">
+					<img src={FrontSide} alt="" />
+				</div>
+			</div>
+		</div>
+
 
 
 
