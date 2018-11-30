ad7d7de948680188236c6c9518ca737e8871d96b
Gabriella <csgabka@users.noreply.github.com>
2018-11-30 10:31:24 +0000

    updated portfolio

--- src/components/MyStory.js
+++ src/components/MyStory.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import '../css/MyStory.css';
 import Profile from '../img/17311080_10211117562268372_3587118879478260351_o.jpg';
-//import firebase from '../firebase.js';
 
 class MyStory extends Component  {
 
@@ -10,13 +9,15 @@ class MyStory extends Component  {
 		return (
 		<div className="story_box" >
 		<h1>Hello and welcome...</h1>
-		<p>My name is Gabi and I am an Entry Level Web Developer. I have created this website as a portfolio work. I have been living in London since 2010, but I was born in Hungary, this is where I graduated as a Computer Engineer(BSc).</p>
-		<img id="profile_pic" src={Profile} alt="profile_pic" />
-		<p>- Hungary is the home of many great scientists such as János Neumann (John von), who invented and developed the first working Digital Computing System. -</p>
-		<p> As a new Graduate, I was specialized in Web and Network technologies. Since then I have not worked in IT, instead - for several reasons - I have chosen Retail Management as a new, exciting and beneficial career field for 8 years. As a new year's resolution of 2018, I have made a decision - to choose a career which gives me a better work-life balance, so I started studying online to refresh my knowledge and catch up with the new trend. I was and still am astonished to see how much has the IT industry improved, and the fact that I can keep up with the change leaves me more and more determined every day. While was working full time as a Retail Manager I spent most of my time studying, coding, learning from different sources, basically from anywhere on the Internet. </p>
-		<p>Web development has become my new life's essence. I find it fascinating when you learn to solve a problem and that makes you learn something else connecting to a different issue. The complexity of creating Web Applications unleashes my creativity, there are no limits.</p>
-		<p>I have done my works using mainly HTML5, CSS3, ES6 and React.JS so far.</p>
-		<p>Apart from this music makes my days even more colorful. I like to sing, also I play the guitar and listen mostly blues and hard rock. I love going to concerts as many as I can. Thank you for visiting my page. If you would like to reach me, please do not hesitate and scroll to the contact me section.</p>
+		<div className="story">
+			<p>My name is Gabi and I am an Entry Level Web Developer. I have created this website as a portfolio work. I have been living in London since 2010, but I was born in Hungary, this is where I graduated as a Computer Engineer(BSc).</p>
+			<img id="profile_pic" src={Profile} alt="profile_pic" />
+			<p>- Hungary is the home of many great scientists such as János Neumann (John von), who invented and developed the first working Digital Computing System. -</p>
+			<p> As a new Graduate, I was specialized in Web and Network technologies. Since then I have not worked in IT, instead - for several reasons - I have chosen Retail Management as a new, exciting and beneficial career field for 8 years. In 2018, I have made a decision - to choose a career which gives me a better work-life balance, so I started studying online to refresh my knowledge and catch up with the new trend. While was working full time as a Retail Manager I spent most of my time studying, coding, learning from different sources, basically from anywhere on the Internet. </p>
+			<p>Web development has become my new life's essence. I find it fascinating when you learn to solve a problem and that makes you learn something else connecting to a different issue. The complexity of creating Web Applications unleashes my creativity, there are no limits.</p>
+			<p>I have done my projects using HTML5, CSS3, Javascript (ES6), React.JS and Firebase so far.</p>
+			<p>Apart from this music makes my days even more colorful. I like to sing, also I play the guitar and listen mostly blues and hard rock. I love going to concerts as many as I can. Thank you for visiting my page. If you would like to reach me, please do not hesitate to contact me.</p>
+		</div>
 		<hr />
 		<button id="scroll-button" type="button" className="btn btn-outline-info" onClick={this.props.scrollToTop}>
 		Scroll me back to the top!</button>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Portfolio1 from '../img/port3_scrs.png';
 import Portfolio2 from '../img/port1_scrs.png';
 import Portfolio3 from '../img/port2_scrs.png';
+import Portfolio4 from '../img/port4_scrs.png';
 import '../css/Portfolio.css';
 
 
@@ -10,28 +11,59 @@ class Portfolio extends Component {
 	constructor() {
 		super();
 		this.state = {
-			images: [Portfolio1, Portfolio2, Portfolio3],
+			portfolios: [
+				{
+					id: 1,
+					image: Portfolio4,
+					description: 'This project is a BLOG app, built in Vanilla JS, backend is provided by Google`s Firebase. It has an administration surface available for all users for testing. When adding new posts, it automatically generates the current time and attaches to each post. The design is dinamic, where the user can choose between day and night mode when clicking on the orb on the left hand side corder.',
+					viewLink: 'https://csgabka-1539976293487.firebaseapp.com/',
+					viewSource: 'https://github.com/csgabka/JS-and-Firebase-Blog-App-with-day-and-night-design',
+				},
+				{
+					id: 2,
+					image: Portfolio3,
+					description: 'This IMAGE GALLERY app was built in React. States were used to create the modal effect and for the navigation as well. Also keyboard navigation is an added functionality.',
+					viewLink: 'https://csgabka.github.io/React-Image-Gallery/',
+					viewSource: 'https://github.com/csgabka/React-Image-Gallery'
+				},
+				{	
+					id: 3, 
+					image: Portfolio1, 
+					description: 'Fetches a random JSON file and at state level orders, filters, adds, removes or edits data from it.',
+					viewLink: 'https://csgabka.github.io/phonebook_interface/',
+					viewSource: 'https://github.com/csgabka/phonebook_interface' 
+				},
+				{
+					id: 4,
+					image: Portfolio2,
+					description: 'The project is my PERSONAL WEB APP. It was built in React. Apart from coding it from scratch, I have used different npm packages to learn how to use those when developing in React.',
+					viewLink: 'https://csgabka.github.io/csgabka-portfolio/',
+					viewSource: 'https://github.com/csgabka/csgabka-portfolio' 
+				},
+			]
 		}
 	}
 
-
 	render() {
-		const { images } = this.state; 
+		const { portfolios } = this.state; 
 		return (
 			<div className="portfolio">
 			<div className="portfolio_header">
 			<h1>My Portfolio</h1>
-			<p className="technologies">Technologies used: HTML5, CSS3, Javascript, React.Js.</p>
+			</div>
 			<div className="image-gallery">
-				{images.map((image, index) => {
-					return <img name={image} className="single-image" src={image} key={image} alt={image}/>
+			<p className="technologies">Technologies used: HTML5, CSS3, Javascript, React.Js., Firebase</p>
+				{portfolios.map((portfolio) => {
+					return (
+					<div>
+					<a href={portfolio.viewLink}><img name={portfolio.image} className="single-image" src={portfolio.image} key={portfolio.image} alt={portfolio.image}/></a>
+					<p className="portfolio-description">
+					{portfolio.description}<br />
+					<a className="source" href={portfolio.viewSource}>View source code</a></p>
+					</div> )
 				})}
 			</div>
-			</div>
 			<div className="portfolio_footer">
-			<p className="portfolio-description"><a href="https://csgabka.github.io/phonebook_interface/">Click here to view</a><br />Fetches a random JSON file and at state level orders, filters, adds, removes or edits data from it.<br /><a className="source" href="https://github.com/csgabka/phonebook_interface">View source code</a></p>
-			<p className="portfolio-description"><a href="https://csgabka.github.io/csgabka-portfolio/">Click here to view</a><br />The project is my personal web app. Apart from coding it from scratch, I have used different npm packages to practice how to plug in those into my React app.<br /><a className="source" href="https://github.com/csgabka/csgabka-portfolio">View source code</a></p>
-			<p className="portfolio-description"><a href="https://csgabka.github.io/React-Image-Gallery/">Click here to view</a><br />States are used to create the modal effect and navigate. When choosing an image, it focuses on it straight, without having to click on it first to enable navigation with the keyboard.<br /><a className="source" href="https://github.com/csgabka/React-Image-Gallery">View source code</a></p>
 			<hr />
 			<button id="scroll-button" type="button" className="btn btn-outline-info" onClick={this.props.scrollToTop}>
 		Scroll me back to the top!</button>
