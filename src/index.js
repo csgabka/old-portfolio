5ea5d10e0ba315b5f1eab96cac96f0aa8b790295
Gabriella <csgabka@users.noreply.github.com>
2018-12-10 12:46:38 +0000

    polyfill

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'compass';
+import 'babel-polyfill';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
